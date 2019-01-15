6bcf8b90c054e984de42f4197d7f758266324dd1
月之领主LM <178910432@qq.com>
2019-01-15 21:39:54 +0800

    Update

---
+++ Server/run.jar.8091.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set "server_port=8091"
+
+set "jar_file=target\server-0.0.1-SNAPSHOT.jar"
+if not exist "target" (
+    mkdir "target"
+    call mvn install
+)
+
+copy /B "%jar_file%" "%jar_file%.%server_port%"
+set "jar_file=%jar_file%.%server_port%"
+
+echo java -jar "%jar_file%" --server.port=%server_port% ^>^>"%~n0.log" 2^>^>^&1
+java -jar "%jar_file%" --server.port=%server_port% >>"%~n0.log" 2>>&1
+
+exit
