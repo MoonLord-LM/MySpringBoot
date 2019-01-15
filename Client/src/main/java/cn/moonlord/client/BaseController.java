9c2c061506fd608b1044d58db0b5bc3a3fcaf741
月之领主LM <178910432@qq.com>
2019-01-15 13:42:02 +0800

    Update

--- Client/src/main/java/cn/moonlord/client/BaseController.java
+++ Client/src/main/java/cn/moonlord/client/BaseController.java
@@ -2,16 +2,13 @@ package cn.moonlord.client;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api
 @RestController
@@ -22,15 +19,25 @@ public class BaseController implements ErrorController {
 
     @ApiOperation(value="基础 index 服务", notes="用于处理根路径的请求")
     @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST} )
-    public String index() {
-        return "This is Index Page. <br/> Hello World!";
+    public String index() throws Exception {
+        return "This is Index Page. <br/>"
+                + "Hello World!";
     }
 
     @ApiOperation(value="基础 error 服务", notes="用于处理出错的请求")
     @RequestMapping(value = "/error", method = { RequestMethod.GET, RequestMethod.POST} )
-    public String error(HttpServletRequest request, HttpServletResponse response) {
-        WebRequest webRequest = new ServletWebRequest(request);
-        return "This is Error Page. <br/> Request URL: " + request.getRequestURI() + "Error Code: " + response.getStatus() + ".";
+    public String error(HttpServletRequest request) {
+        String request_uri = (String) request.getAttribute("javax.servlet.error.request_uri");
+        if (request_uri == null){
+            request_uri = request.getRequestURI();
+        }
+        Integer status_code = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (status_code == null){
+            status_code = HttpStatus.OK.value();
+        }
+        return "This is Error Page. <br/>"
+                + "Request URI: " + request_uri + " <br/>"
+                + "Error Code: " + status_code + " <br/>";
     }
 
 }
