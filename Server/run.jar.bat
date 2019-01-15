84bb50d865d254cd8c27f714886cc7762ee7503b
月之领主LM <178910432@qq.com>
2019-01-15 20:06:34 +0800

    Update

---
+++ Server/run.jar.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set "jar_file=target\server-0.0.1-SNAPSHOT.jar"
+if not exist "target" (
+    mkdir "target"
+    call mvn install
+)
+
+echo java -jar "%jar_file%" ^>^>"%~n0.log" 2^>^>^&1
+java -jar "%jar_file%" >>"%~n0.log" 2>>&1
+
+exit
