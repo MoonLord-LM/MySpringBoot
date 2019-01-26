aa7584a3d48bb74f9680d99f0782caed91ce96f0
月之领主LM <178910432@qq.com>
2019-01-26 18:01:19 +0800

    Update

--- clean.bat
+++ clean.bat
@@ -1,19 +1,14 @@
 @echo off
 
-cd "Client"
-rmdir /S /Q "target"
-rmdir /S /Q "logs"
-del /F /S /Q "*.log"
-
-cd "../Server"
-rmdir /S /Q "target"
-rmdir /S /Q "logs"
-del /F /S /Q "*.log"
-
-cd "../Starter"
-rmdir /S /Q "target"
-rmdir /S /Q "logs"
-del /F /S /Q "*.log"
+cd "Client" && call :clean && cd ..
+cd "Server" && call :clean && cd ..
+cd "Starter" && call :clean && cd ..
 
 pause
 exit
+
+:clean - 清除编译结果和日志
+    rmdir /S /Q "target"
+    rmdir /S /Q "logs"
+    del /F /S /Q "*.log"
+goto :eof
