7a68e958946c6165943510215da6b727e0efa87b
月之领主LM <178910432@qq.com>
2019-04-01 00:33:54 +0800

    Update

--- Server/src/main/java/cn/moonlord/server/ServerApplication.java
+++ Server/src/main/java/cn/moonlord/server/ServerApplication.java
@@ -3,12 +3,14 @@ package cn.moonlord.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableAdminServer
 @EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 public class ServerApplication {
 
