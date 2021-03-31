b3ba7e79b3fe5f384b6d9e4235f3b43f55aea186
月之领主LM <178910432@qq.com>
2021-03-31 23:25:43 +0800

    Update

---
+++ client/src/test/java/cn/moonlord/springboot/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.moonlord.springboot.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
