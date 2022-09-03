4669fbe9a72f8b60394088f91da4e58d2183cadb
MoonLord-LM <me@moonlord.cn>
2022-09-03 21:47:42 +0800

    Update

---
+++ spring-boot-starter-web/src/main/java/cn/moonlord/springbootstarterweb/SpringBootStarterWebApplication.java
@@ -0,0 +1,13 @@
+package cn.moonlord.springbootstarterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterWebApplication.class, args);
+    }
+
+}
