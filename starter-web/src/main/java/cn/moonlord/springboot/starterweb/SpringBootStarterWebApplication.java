ee181d91276919df869a1591a67dc155146fcf7b
MoonLord-LM <me@moonlord.cn>
2022-09-03 21:58:12 +0800

    Update

--- starter-web/src/main/java/cn/moonlord/springboot/starterweb/SpringBootStarterWebApplication.java
+++ starter-web/src/main/java/cn/moonlord/springboot/starterweb/SpringBootStarterWebApplication.java
@@ -2,12 +2,21 @@ package cn.moonlord.springboot.starterweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootStarterWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarterWebApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
