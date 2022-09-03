2754b773924f320cff2c87d665f8e8239513387c
MoonLord-LM <me@moonlord.cn>
2022-09-03 23:18:29 +0800

    Update

---
+++ spring-cloud-config-server/src/main/java/cn/moonlord/springboot/Application.java
@@ -0,0 +1,24 @@
+package cn.moonlord.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableConfigServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
