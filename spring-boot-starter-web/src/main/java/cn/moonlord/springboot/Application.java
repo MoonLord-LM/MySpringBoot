c5508d4d04fa7f01b99c8597d16a6dee03be9010
MoonLord-LM <me@moonlord.cn>
2022-09-04 23:09:19 +0800

    Update

--- spring-boot-starter-web/src/main/java/cn/moonlord/springboot/Application.java
+++ spring-boot-starter-web/src/main/java/cn/moonlord/springboot/Application.java
@@ -1,5 +1,7 @@
 package cn.moonlord.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @RequestMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+        logger.info("hello {}", name);
+        return String.format("hello %s", name);
     }
 
 }
