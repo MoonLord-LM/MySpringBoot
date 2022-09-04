5e76b8995496fa2b7c653fe0d3514995c6a1ab24
MoonLord-LM <me@moonlord.cn>
2022-09-04 22:35:40 +0800

    Update

--- dev.bat
+++ dev.bat
@@ -15,7 +15,8 @@ start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.
  --spring.application.name=spring-cloud-eureka-server ^
  --management.endpoints.web.exposure.include=* ^
  --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
- --eureka.client.fetch-registry=true --eureka.client.register-with-eureka=true ^
+ --eureka.client.fetch-registry=true ^
+ --eureka.client.register-with-eureka=true ^
  --eureka.instance.hostname=localhost
 cd "../"
 
@@ -24,14 +25,19 @@ start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.
  --spring.application.name=spring-cloud-eureka-server ^
  --management.endpoints.web.exposure.include=* ^
  --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
- --eureka.client.fetch-registry=true --eureka.client.register-with-eureka=true ^
+ --eureka.client.fetch-registry=true ^
+ --eureka.client.register-with-eureka=true ^
  --eureka.instance.hostname=127.0.0.1
 cd "../"
 
 cd "spring-cloud-gateway-server"
 start java -jar "target/spring-cloud-gateway-server-0.0.1-SNAPSHOT.jar" --server.port=8080 ^
  --spring.application.name=spring-cloud-gateway-server ^
- --management.endpoints.web.exposure.include=*
+ --management.endpoints.web.exposure.include=* ^
+ --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
+ --eureka.client.fetch-registry=true ^
+ --eureka.client.register-with-eureka=true ^
+ --eureka.instance.hostname=localhost
 cd "../"
 
 pause
--- spring-cloud-gateway-server/src/main/java/cn/moonlord/springboot/Application.java
+++ spring-cloud-gateway-server/src/main/java/cn/moonlord/springboot/Application.java
@@ -2,8 +2,11 @@ package cn.moonlord.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
