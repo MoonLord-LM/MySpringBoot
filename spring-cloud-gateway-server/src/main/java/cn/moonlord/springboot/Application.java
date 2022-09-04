54e242b9385fae24b6edda9b014102a32a790fc9
MoonLord-LM <me@moonlord.cn>
2022-09-04 23:36:56 +0800

    Update

--- spring-cloud-gateway-server/src/main/java/cn/moonlord/springboot/Application.java
+++ spring-cloud-gateway-server/src/main/java/cn/moonlord/springboot/Application.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.*;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
