9a885b810b5481252c39f0d01aa61873a77c7579
MoonLord-LM <me@moonlord.cn>
2022-09-05 00:49:16 +0800

    Update

--- dev.bat
+++ dev.bat
@@ -5,39 +5,32 @@ cd "spring-cloud-eureka-server" && call mvn clean package && cd "../"
 cd "spring-cloud-gateway-server" && call mvn clean package && cd "../"
 
 cd "spring-cloud-config-server"
-start java -jar "target/spring-cloud-config-server-0.0.1-SNAPSHOT.jar" --server.port=9000 ^
- --spring.application.name=spring-cloud-config-server ^
+start java -jar "target/spring-cloud-config-server-0.0.1-SNAPSHOT.jar" --server.port=8888 ^
  --management.endpoints.web.exposure.include=*
 cd "../"
 
 cd "spring-cloud-eureka-server"
-start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.port=9001 ^
- --spring.application.name=spring-cloud-eureka-server ^
+start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.port=8761 ^
  --management.endpoints.web.exposure.include=* ^
- --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
+ --eureka.client.service-url.defaultZone=http://localhost:8761/eureka/,http://127.0.0.1:8762/eureka/ ^
  --eureka.client.fetch-registry=true ^
  --eureka.client.register-with-eureka=true ^
  --eureka.instance.hostname=localhost
 cd "../"
 
 cd "spring-cloud-eureka-server"
-start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.port=9002 ^
- --spring.application.name=spring-cloud-eureka-server ^
+start java -jar "target/spring-cloud-eureka-server-0.0.1-SNAPSHOT.jar" --server.port=8762 ^
  --management.endpoints.web.exposure.include=* ^
- --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
+ --eureka.client.service-url.defaultZone=http://localhost:8761/eureka/,http://127.0.0.1:8762/eureka/ ^
  --eureka.client.fetch-registry=true ^
  --eureka.client.register-with-eureka=true ^
  --eureka.instance.hostname=127.0.0.1
 cd "../"
 
 cd "spring-cloud-gateway-server"
-start java -jar "target/spring-cloud-gateway-server-0.0.1-SNAPSHOT.jar" --server.port=8080 ^
- --spring.application.name=spring-cloud-gateway-server ^
+start java -jar "target/spring-cloud-gateway-server-0.0.1-SNAPSHOT.jar" --server.port=80 ^
  --management.endpoints.web.exposure.include=* ^
- --eureka.client.service-url.defaultZone=http://localhost:9001/eureka/,http://127.0.0.1:9002/eureka/ ^
- --eureka.client.fetch-registry=true ^
- --eureka.client.register-with-eureka=true ^
- --eureka.instance.hostname=localhost
+ --eureka.client.service-url.defaultZone=http://localhost:8761/eureka/,http://127.0.0.1:8762/eureka/
 cd "../"
 
 pause
