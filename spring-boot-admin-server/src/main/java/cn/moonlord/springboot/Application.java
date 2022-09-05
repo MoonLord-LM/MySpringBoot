49245838e8d9c126af89e6a945505edc3c2f1d8e
MoonLord-LM <me@moonlord.cn>
2022-09-05 23:57:34 +0800

    Update Application.java

--- spring-boot-admin-server/src/main/java/cn/moonlord/springboot/Application.java
+++ spring-boot-admin-server/src/main/java/cn/moonlord/springboot/Application.java
@@ -1,11 +1,13 @@
 package cn.moonlord.springboot;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.web.client.InstanceExchangeFilterFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -16,6 +18,13 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public InstanceExchangeFilterFunction requestFilter() {
+        return (instance, request, next) -> next.exchange(request).doOnSubscribe((s) -> {
+            logger.info("requestFilter method: {} instance: {} url: {}", request.method(), instance.getId(), request.url());
+        });
     }
 
 }
