b15d1cd725e9850c9173c948c9122e5e999f2fc2
MoonLord-LM <me@moonlord.cn>
2022-09-07 03:08:15 +0800

    Update

--- spring-boot-admin-server/src/main/java/cn/moonlord/springboot/Application.java
+++ spring-boot-admin-server/src/main/java/cn/moonlord/springboot/Application.java
@@ -1,26 +1,11 @@
 package cn.moonlord.springboot;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import de.codecentric.boot.admin.server.domain.entities.Instance;
-import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
-import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
-import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
-import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
-import de.codecentric.boot.admin.server.web.client.InstanceExchangeFilterFunction;
-import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
-import reactor.core.publisher.Mono;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 @EnableAdminServer
