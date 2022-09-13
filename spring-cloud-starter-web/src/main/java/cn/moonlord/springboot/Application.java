6ce3ddfb95079cb0f31a24aef28a4188996c0a46
MoonLord-LM <178910432@qq.com>
2022-09-13 16:19:18 +0800

    Update

---
+++ spring-cloud-starter-web/src/main/java/cn/moonlord/springboot/Application.java
@@ -0,0 +1,34 @@
+package cn.moonlord.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public Map<String, String> hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+        logger.info("hello {}", name);
+        HashMap<String, String> result = new HashMap<>();
+        result.put("hello", name);
+        return result;
+    }
+
+}
